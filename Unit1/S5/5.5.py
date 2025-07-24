72ac419735b374b969d90dfd95736c5c9afb819d
Cristian <cristianmatera2001@gmail.com>
2025-07-24 06:22:01 -0400

    up

--- Unit1/S5/5.5.py
+++
@@ -1,8 +0,0 @@
-import http.client
-host = input("inserire ip/host del sistema target:  ")
-port = input("inserire porta del sistema target (default 80):  ")
-if(port == ""):
-    port = 80
-try:
-    connection = http.client.HTTPConnection(host, port)
-    connection.request("OPTIONS", "/")
